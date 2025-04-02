92142e1f13c34fba0827534a8769e8a294ea356a
Victoria Breen <victoriabreen8@hotmail.com>
2025-04-02 15:51:22 -0230

    Working on the TreeService

--- src/main/java/org/keyin/tree/TreeService.java
+++ src/main/java/org/keyin/tree/TreeService.java
@@ -1,4 +1,33 @@
 package org.keyin.tree;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TreeService {
+
+    private  final TreeRepository treeRepository;
+    // double check the mapper to ensure it works
+    private final ObjectMapper objectmapper; // should make converting to json easier
+
+    @Autowired
+    public TreeService(TreeRepository treeRepository, ObjectMapper objectmapper){
+        this.treeRepository = treeRepository;
+         this.objectmapper = objectmapper;
+    }
+
+    // save tree records
+    public Tree saveTree(String inputNum, TreeNode treeNode) throws Exception {
+        String treeJSON = objectmapper.writeValueAsString(treeNode);
+        Tree tree = new Tree(inputNum, treeJSON);
+        return treeRepository.save(tree);
+    }
+
+    // save input numbers
+    // make info (node into json - mapper
+    // get all of the trees
+    // create tree from a list of int
+    // BONUS: balanced tree to the user
+
 }
